c0f4b109fe4d31defe13eaf39b12a5a25966a922
Your Name <you@example.com>
2026-03-18 10:08:09 +0000

    deps

--- backend/run_tests.sh
+++ backend/run_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd "$(dirname "$(readlink -f -- "$0")")"
-
+sudo apt install -qy libimage-exiftool-perl
 ./api/run_unit_tests.sh && ./worker/run_unit_tests.sh && ./tests/run_integration_tests.sh
