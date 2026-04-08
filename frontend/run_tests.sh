bfd61c963edbd7cbded9c62e0113f60704302f6e
Jindrich Kolman <kolman.jindrich@gmail.com>
2026-04-08 06:51:30 +0000

    ..

--- frontend/run_tests.sh
+++ frontend/run_tests.sh
@@ -1,5 +1,11 @@
-#!/bin/bash
+#!/bin/fish
 cd "$(dirname "$(readlink -f -- "$0")")"
 
-./run_unit_tests.sh && ./run_playwright_tests.sh 
-# && ./run_appium_tests.sh
+./run_unit_tests.sh;
+and ./run_playwright_tests.sh
+and begin
+	if test "$ANDROID" != "0"; then
+		./run_appium_tests.sh;
+	end
+end
+
